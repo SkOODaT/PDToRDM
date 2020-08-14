c53b1d3c1390985092c5ee757e158fc6d897baa8
SkOODaT <8633932+SkOODaT@users.noreply.github.com>
2020-08-14 01:52:48 -0400

    Project Rename + Changes
    Rename Project To reflect Use Better
    Handle Disconnected Errors
    Moved Some Code UP+ Changed Header

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Mad To RDM
+# PD To RDM
 
 Forwards Pogodroid Raw Data to RDM. 
 Limits Requests Types To Only What RDM Can Handle. (Massive RDM Performance Improvement)
@@ -7,10 +7,10 @@ Adds RDM Header Token.
 
 <strong>The contents of this repo is a proof of concept and is for educational use only!</strong>
 
-## Mad To RDM Config
+## PD To RDM Config
 
 - Enable Send Raw Data In Pogodroid
 - Point Pogodroid Raw To Connector Address
-- Add RDM Endpoint In MadToRDM.py: RDM_URL = "http://youserver:5000/raw"
-- Add RDM Token to MadToRDM.py: AUTH_TOKEN = 'YOURRDMTOKEN'
-- Startup.... python madtordm.py (python3)
+- Add RDM Endpoint In PDToRDM.py: RDM_URL = "http://youserver:5000/raw"
+- Add RDM Token to PDToRDM.py: AUTH_TOKEN = 'YOURRDMTOKEN'
+- Startup.... python pdtordm.py (python3)
---
+++ pdtordm.py
@@ -0,0 +1,58 @@
+from flask import Flask, request
+import requests, os
+
+app = Flask(__name__, static_url_path='')
+
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+RDM_URL = "192.168.1.100:9001"
+AUTH_TOKEN = 'YOURRDMTOKEN'
+
+headers = {
+    "User-Agent": "Python PDToRDM Connector V1.1",
+    "Authorization": "Bearer " + AUTH_TOKEN
+}
+
+@app.route("/raw", methods=["POST"])
+def raw():
+    try:
+        data = request.get_json(force=True)
+        unique_id = request.headers.get('Origin')
+        ip_address = request.environ['REMOTE_ADDR']
+        user_agent = request.user_agent.string
+        if isinstance(data, list):
+            for proto in data:
+                method = proto["type"]
+                #print("[PDTORDM] %s - %s - %s" %(unique_id, ip_address, user_agent))
+                #print("[PDTORDM] %s - %s" %(unique_id, method))
+                if method == 2 or method == 106 or method == 102 or method == 104 or method == 101 or method == 156:
+                    print("[PDTORDM] %s - %s - %s" %(unique_id, ip_address, user_agent))
+                    print("[PDTORDM] %s - %s" %(unique_id, method))
+                    req_rdm = handle_proto_data(proto, unique_id)
+                    req = requests.post(url="http://"+RDM_URL"/raw", json=req_rdm, headers=headers)
+                    if req.status_code not in [200,201]:
+                       print("[PDTORDM] Status code: {}".format(req.status_code))
+        else:
+            print("[PDTORDM] PogoDroid Protos Error")
+    except requests.exceptions.ConnectionError as e:
+        retry_error = True
+        print("[PDTORDM] ERROR", e)
+    return 'OK'
+
+def handle_proto_data(proto, unique_id):
+    # pull required data for rdm/ Trick rdm
+    req_data = {}
+    req_data["data"] = proto.pop("payload")
+    req_data["method"] = proto.pop("type")
+    # format data to rdm
+    req_rdm = {"contents": []}
+    req_rdm["contents"].append(req_data)
+    req_rdm["username"] = unique_id
+    req_rdm["trainerlvl"] = int(30)
+    req_rdm["uuid"] = unique_id
+    return req_rdm
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port='5000', debug=True)
